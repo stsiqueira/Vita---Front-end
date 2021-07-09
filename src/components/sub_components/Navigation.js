5b04ffad8636aefef0760f932443e87a6c1a6a22
Thiago <stsiqueira@outlook.com>
2021-07-09 07:30:29 -0700

    fix the navigation links #62
    fix the navigation links #62

--- src/components/sub_components/Navigation.js
+++ src/components/sub_components/Navigation.js
@@ -15,22 +15,22 @@ const Navigation = (props) => {
        <div className={props.showMenu ? "navigation mobileMenu" : "navigation"}>
            <ul>
                 <li>
-                    <Link to='/NutrientCalculator' onClick={()=> props.setShowMenu(!props.showMenu)}>
+                    <Link to='/NutrientCalculator' onClick={()=> props.setShowMenu(false)}>
                         Nutrient Calculator
                     </Link>
                 </li>
                 <li>
-                    <Link to='Quiz' onClick={()=> props.setShowMenu(!props.showMenu)}> 
+                    <Link to='Quiz' onClick={()=> props.setShowMenu(false)}> 
                         Quiz
                     </Link>   
                 </li>
                 <li>
-                    <Link to='Contact' onClick={()=> props.setShowMenu(!props.showMenu)}>
+                    <Link to='Contact' onClick={()=> props.setShowMenu(false)}>
                         Contact
                     </Link>
                 </li>
                 <li>
-                    <Link to='Team' onClick={()=> props.setShowMenu(!props.showMenu)}> 
+                    <Link to='Team' onClick={()=> props.setShowMenu(false)}> 
                         Team
                     </Link>        
                 </li>
