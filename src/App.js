8e8777eeeecd3274df3f0dce2db5bea134d7f374
glen thomas <glenkt2@gmail.com>
2021-07-11 08:12:48 -0700

    added description/#77

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
             <NotFound />{/*****************  NUTRIENT CALCULATOR RESULT COMPONENT  ***************************/}
           </>
         )}/>
-       </Switch> 
+      </Switch> 
 
       </div>
 
