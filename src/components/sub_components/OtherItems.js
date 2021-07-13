e31c9af11d86a6ba1a9c2b6030972f4b4357d794
factorcode <77560128+factorcode@users.noreply.github.com>
2021-07-13 13:58:19 -0700

    Other Nutrient Headers/#75

--- src/components/sub_components/OtherItems.js
+++ src/components/sub_components/OtherItems.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 
-const OtherItems = ({heading, otherItemsList}) => {
-    
+const OtherItems = ({ heading, otherItemsList }) => {
+
+    const [headDescription, SetHeadDescription] = useState("");
+
+    //FirstLoad - Check if data is sent or not
+    useEffect(() => {
+        (async function () {
+
+            if (heading.includes("Vitamins"))
+                SetHeadDescription("Vitamins are organic molecules that are essential micronutrients which an organism needs in small quantities for the proper functioning of its metabolism.")
+            else if (heading.includes("Minerals"))
+                SetHeadDescription("Minerals are chemical elements required as an essential nutrient by organisms to perform functions necessary for life.");
+            else SetHeadDescription("");
+
+        })();
+    }, [heading]);
+
+
     let itemImageUrl = "/img/";
     return (
         <div>
             <h2>{heading}</h2>
+            <div className="headDescription">
+                {headDescription}
+            </div>
             <div className="otherItems">
                 {
                     otherItemsList && otherItemsList.map((item, key) => (
