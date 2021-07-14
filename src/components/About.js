2cb497fccb2c4b1db36a78918d45d1b1776f6582
Thiago <stsiqueira@outlook.com>
2021-07-13 22:27:32 -0700

    style legends
    style legends on home page
    update home image

--- src/components/About.js
+++ src/components/About.js
@@ -11,11 +11,11 @@ const About = (props) => {
     return (
        <div className="about">
           
-            <h2> What is Vita?</h2>
+            <h2> Welcome to Vita!</h2>
             <div className="description">
                 <p>Vita is a data-driven responsive web platform educating users about vitamins and minerals present in natural food sources. Vita has a built-in nutrition calculator to suggest vegetables and fruits that can help maintain a balanced diet</p>
                 <Link to="/NutrientCalculator">
-                    <button className="btn btnCalculateHome"> Calculate</button>
+                    <button className="btn btnCalculateHome"> Try it out</button>
                 </Link>
 
            </div>
